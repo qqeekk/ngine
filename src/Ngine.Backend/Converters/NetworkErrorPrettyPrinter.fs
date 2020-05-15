a159cf85cd34cf84dfa1d5f87bd20a00c7a479ee
Alexander Drozdov <drozdov.alexander.98@gmail.com>
2020-05-15 21:48:17 +0700

    fix

--- src/Ngine.Backend/Converters/NetworkErrorPrettyPrinter.fs
+++ src/Ngine.Backend/Converters/NetworkErrorPrettyPrinter.fs
@@ -33,32 +33,32 @@ module NetworkErrorPrettyPrinter =
                 match e with
                 | LayerCompatibilityError.DimensionMissmatch ->
                     String.Format("Layer {0} of type {1} does not match {2} of type {3} by dimensions",
-                        sprintf "%A" id, l1.Type, sprintf "%A" id2, l2.Type), []
+                        LayerIdEncoder.encoder.encode id, l1.Type, LayerIdEncoder.encoder.encode id2, l2.Type), []
 
                 | LayerCompatibilityError.DuplicateLayerId ->
-                    String.Format("Layer id {0} is ambiguous", sprintf "%A" id), []
+                    String.Format("Layer id {0} is ambiguous", LayerIdEncoder.encoder.encode id), []
 
                 |> Seq.singleton
                 
             | LayerError.LayerError es -> 
                 es |> Seq.map (function
                     | LayerConversionError.UnknownType ty ->
-                        String.Format("Unknown type for layer {0} - {1}", sprintf "%A" id, ty), []
+                        String.Format("Unknown type for layer {0} - {1}", LayerIdEncoder.encoder.encode id, ty), []
                     
                     | LayerConversionError.InvalidAmbiguity (Variable var) ->
                         String.Format("Invalid variable reference: {0}", var), []
 
                     | LayerConversionError.ExpectedLayerId ->
-                        String.Format("Layer {0} not connected with any layer", sprintf "%A" id), []
+                        String.Format("Layer {0} not connected with any layer", LayerIdEncoder.encoder.encode id), []
                     
                     | LayerConversionError.MissingLayerId lid ->
-                        String.Format("Layer {0} not found", sprintf "%A" lid), []
+                        String.Format("Layer {0} not found", LayerIdEncoder.encoder.encode lid), []
                     
                     | LayerConversionError.PrevLayerPropsEmpty ->
-                        String.Format("Layer {0} props not filled with concatenated layer ids not found", sprintf "%A" id), []
+                        String.Format("Layer {0} props not filled with concatenated layer ids not found", LayerIdEncoder.encoder.encode id), []
                     
                     | LayerConversionError.PropsConversionError pe ->
-                        String.Format("Error while converting props for layer {0}", sprintf "%A" id),
+                        String.Format("Error while converting props for layer {0}", LayerIdEncoder.encoder.encode id),
                         [Node (prettifyPropsConversionError pe)]
                 )
                 
@@ -86,7 +86,7 @@ module NetworkErrorPrettyPrinter =
 
             | HeadError.HeadFunctionError p -> seq { prettifyPropsConversionError p }
             | HeadError.LossError (LossError.UnknownType) ->
-                seq { String.Format("Unknown loss type for layer {0} - {1}", sprintf "%A" h.LayerId, h.Loss), [] })
+                seq { String.Format("Unknown loss type for layer {0} - {1}", LayerIdEncoder.encoder.encode h.LayerId, h.Loss), [] })
 
         | OptimizerError p -> seq { prettifyPropsConversionError p })
         |> Seq.map Node
